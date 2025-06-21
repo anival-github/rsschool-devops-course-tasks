846a261e5bd43dc03d4d6d74dcb06228c20e08f6
Admin <mr.valentin.anikin@gmail.com>
2025-06-21 22:14:19 +0300

    task_2

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -39,7 +39,7 @@ resource "aws_Security_group" "private" {
     from_port = 0
     to_port = 0
     protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
