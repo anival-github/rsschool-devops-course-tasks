e95194801158de5fc3cea403a97574d3ad9519f4
Admin <mr.valentin.anikin@gmail.com>
2025-06-29 00:07:08 +0300

    task_3

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -17,7 +17,7 @@ resource "aws_key_pair" "k8s" {
 }
 
 resource "aws_instance" "k8s_master" {
-  ami                         = data.aws_ami.ubuntu.id
+  ami                         = "ami-020cba7c55df1f615"
   instance_type               = var.k8s_master_instance_type
   subnet_id                   = aws_subnet.private[0].id
   key_name                    = aws_key_pair.k8s.key_name
@@ -26,12 +26,33 @@ resource "aws_instance" "k8s_master" {
   user_data_replace_on_change = true
 
   user_data = <<-EOF
-#!/bin/bash
-apt-get update -y
-apt-get install -y curl
-PRIVATE_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
-curl -sfL https://get.k3s.io | sh -s - server --cluster-init --token ${var.k3s_token} --node-ip $PRIVATE_IP --write-kubeconfig-mode=644
-EOF
+              #!/bin/bash
+              exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+              set -x
+
+              echo "--- STARTING USER_DATA SCRIPT ---"
+              date
+
+              echo "--> Running apt-get update"
+              apt-get update -y
+              if [ $? -ne 0 ]; then echo "APT-GET UPDATE FAILED"; exit 1; fi
+              
+              echo "--> Running apt-get install curl"
+              apt-get install -y curl
+              if [ $? -ne 0 ]; then echo "APT-GET INSTALL FAILED"; exit 1; fi
+
+              echo "--> Getting private IP using IMDSv2"
+              TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+              PRIVATE_IP=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/local-ipv4)
+              echo "--> Private IP is $PRIVATE_IP"
+
+              echo "--> Running k3s install script"
+              curl -sfL https://get.k3s.io | sh -s - server --cluster-init --token ${var.k3s_token} --node-ip $PRIVATE_IP --write-kubeconfig-mode=644
+              if [ $? -ne 0 ]; then echo "K3S INSTALL FAILED"; exit 1; fi
+
+              echo "--- FINISHED USER_DATA SCRIPT ---"
+              date
+              EOF
 
   tags = {
     Name = "k8s-master"
@@ -39,7 +60,7 @@ EOF
 }
 
 resource "aws_instance" "k8s_worker" {
-  ami                         = data.aws_ami.ubuntu.id
+  ami                         = "ami-020cba7c55df1f615"
   instance_type               = var.k8s_worker_instance_type
   subnet_id                   = aws_subnet.private[1].id
   key_name                    = aws_key_pair.k8s.key_name
@@ -48,11 +69,31 @@ resource "aws_instance" "k8s_worker" {
   user_data_replace_on_change = true
 
   user_data = <<-EOF
-#!/bin/bash
-apt-get update -y
-apt-get install -y curl
-curl -sfL https://get.k3s.io | K3S_URL="https://${aws_instance.k8s_master.private_ip}:6443" K3S_TOKEN="${var.k3s_token}" sh -
-EOF
+              #!/bin/bash
+              exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+              set -x
+
+              echo "--- STARTING USER_DATA SCRIPT ---"
+              date
+
+              echo "--> Waiting for k8s master to be ready"
+              until ping -c1 ${aws_instance.k8s_master.private_ip}; do sleep 10; done
+
+              echo "--> Running apt-get update"
+              apt-get update -y
+              if [ $? -ne 0 ]; then echo "APT-GET UPDATE FAILED"; exit 1; fi
+
+              echo "--> Running apt-get install curl"
+              apt-get install -y curl
+              if [ $? -ne 0 ]; then echo "APT-GET INSTALL FAILED"; exit 1; fi
+
+              echo "--> Running k3s worker install script"
+              curl -sfL https://get.k3s.io | K3S_URL="https://${aws_instance.k8s_master.private_ip}:6443" K3S_TOKEN="${var.k3s_token}" sh -
+              if [ $? -ne 0 ]; then echo "K3S WORKER INSTALL FAILED"; exit 1; fi
+
+              echo "--- FINISHED USER_DATA SCRIPT ---"
+              date
+              EOF
 
   tags = {
     Name = "k8s-worker"
--- terraform/network_acls.tf
+++ terraform/network_acls.tf
@@ -10,9 +10,20 @@ resource "aws_network_acl" "public" {
     to_port    = 22
   }
 
+  # Allow traffic from private subnets to the NAT Gateway
+  ingress {
+    protocol   = "-1"
+    rule_no    = 110
+    action     = "allow"
+    cidr_block = var.vpc_cidr
+    from_port  = 0
+    to_port    = 0
+  }
+
+  # Allow return traffic to bastion on ephemeral ports
   ingress {
     protocol   = "tcp"
-    rule_no    = 110
+    rule_no    = 120
     action     = "allow"
     cidr_block = "0.0.0.0/0"
     from_port  = 1024
@@ -36,6 +47,7 @@ resource "aws_network_acl" "public" {
 resource "aws_network_acl" "private" {
   vpc_id = aws_vpc.main.id
 
+  # Allow all traffic from the VPC (e.g. from bastion, other nodes)
   ingress {
     protocol   = "-1"
     rule_no    = 100
@@ -45,6 +57,7 @@ resource "aws_network_acl" "private" {
     to_port    = 0
   }
 
+  # Allow return traffic from the internet on ephemeral ports
   ingress {
     protocol   = "tcp"
     rule_no    = 110
@@ -54,31 +67,14 @@ resource "aws_network_acl" "private" {
     to_port    = 65535
   }
 
+  # Allow all outbound traffic to the internet
   egress {
     protocol   = "-1"
     rule_no    = 100
     action     = "allow"
-    cidr_block = var.vpc_cidr
+    cidr_block = "0.0.0.0/0"
     from_port  = 0
     to_port    = 0
-  }
-
-  egress {
-    protocol   = "tcp"
-    rule_no    = 110
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 80
-    to_port    = 80
-  }
-
-  egress {
-    protocol   = "tcp"
-    rule_no    = 120
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 443
-    to_port    = 443
   }
 
   tags = {
@@ -96,4 +92,4 @@ resource "aws_network_acl_association" "private" {
   count          = length(aws_subnet.private)
   network_acl_id = aws_network_acl.private.id
   subnet_id      = aws_subnet.private[count.index].id
-} 
+}
